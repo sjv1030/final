d1059d52641c6231e170b5b196768c88136fef1c
Silverio Vasquez <sjv1030@gmail.com>
2018-05-19 18:14:45 -0400

    model updates

---
+++ svm.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 19 18:00:07 2018
+
+@author: sjv1030_hp
+"""
+
+from py_scripts import get_Data_old
+
+data = get_Data_old.getData(sym='ng',freq='m',eco=1)
