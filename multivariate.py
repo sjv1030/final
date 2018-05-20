a1e52ce8725d0976ee298fd5f0d3b0497a4f6d8f
sjv1030 <31420197+sjv1030@users.noreply.github.com>
2018-05-19 21:43:34 -0400

    Update multivariate.py

--- multivariate.py
+++ multivariate.py
@@ -11,10 +11,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 import pandas as pd
 
-###################################
-## This is supposed  to be imported
 from py_scripts import get_Data_old
-###################################
 
 ## Get data
 data = get_Data_old.getData(sym='ng',freq='m',eco=1)
